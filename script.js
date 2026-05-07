a1f5f6250de502d6248b310dd874ea8b322c0f4c
MohamedThanveer1 <139548444+MohamedThanveer1@users.noreply.github.com>
2026-05-07 23:27:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,230 @@
+// Section 1: Variables (1–8)
+
+// 1 What is the output?
+// var a = 10;
+// a = 15;
+
+// console.log(a);
+// Ans : 15 
+
+// 2 What happens here?
+// let b = 20;
+// let b = 30;
+
+// console.log(b);
+// Ans : Syntax error
+
+// 3 What is the output?
+// const c = 50;
+
+// console.log(c);
+// Ans : 50
+
+// 4.Identify Error
+// const x = 10;
+// x = 20;
+
+// console.log(x);
+// Ans TypeError  
+
+// 5.
+// var p = 5;
+// var p = 10;
+
+// console.log(p);
+// Ans 10
+
+// 6.
+// let q = 25;
+// q = q + 10;
+
+// console.log(q);
+// Ans 35
+
+// 7.
+// const r = 7;
+
+// console.log(r + 3);
+// Ans 10
+
+// 8.Fill corect keyword
+// _ a = 10;
+// Ans let or const or var
+
+// Section 2: Console Methods (9–12)
+
+//9. which method prints normal output?
+// Ans console.log()
+
+// 10.Which method shows warning?
+// Ans console.warn();
+
+// 11.Which method shows error?
+// Ans console.error();
+
+// 12.What does this do?
+//Ans console.clear(a) Thise clear the data and print No Value
+
+// Section 3: Data Types (13–18)
+
+// 13.
+// let a = "hello";
+// console.log(typeof a);
+
+// Ans: string
+
+// 14.
+// let b = 100;
+// console.log(typeof b);
+
+// Ans number
+
+// 15.
+// let c = false;
+// console.log(typeof c);
+
+// Ans boolean
+
+// 16.What is type of null?
+// let d;
+// console.log(d);
+
+// Ans object
+
+// 17.Convert string to number:
+
+// let a= "25";
+
+// Ans console.log(Number(a));
+
+// Section 4: Arrays (19–24)
+
+//19. Create array of 3 fruits.
+// Output?
+
+// Ans 
+//  let arr = ["apple","banana","papaya"];
+
+//20.output
+// let arr = ["a","b","c"];
+// console.log(arr[1]);
+
+// Ans b
+
+// 21.output
+// let arr = ["x","y","z"];
+// console.log(arr[arr.length-1]);
+
+// Ans z
+
+// 22.How to get first element?
+// Ans console.log(arr[0]); 
+
+// 23.Output?
+// let arr = ["apple","banana"];
+// console.log(arr.length);
+
+// Ans 2
+
+// 24.Add new element to array.
+// Ans arr.push("mango")
+
+// section 5:Objects(25-28)
+// 25.create object with name and age
+// let oject = {name:"Mohamed Thanveer.M",
+//     age:22}
+
+// 26.output?
+// let object = {name:"Thanveer"};
+// console.log(object.name);
+
+// 27.output?
+// let obj = {
+//     fruits: ["apple","banana"]
+// };
+// console.log(obj.fruits[1]);
+
+// Ans banana
+
+// 28.How to access last element inside object array?
+// obj. 
+// let obj = {
+//     fruits: ["apple","banana"]
+// };
+
+// Ans let item = obj.fruits[obj.fruits.length-1] 
+//  console.log(item);
+
+// section6.Arithmetic Operators(29-32)
+
+// 29.output?
+// console.log(5+3);
+
+// Ans 8
+
+// 30.output?
+// console.log(10%3);
+ 
+// Ans 1 
+
+// 31.output?
+// console.log(2**3);
+
+// Ans 8
+
+// 32.output?
+// console.log(10/2);
+ 
+// Ans 5
+
+// section 7.Increment/Decrement(33-36)
+// 33.output
+
+// let a = 5;
+// a++;
+// console.log(a);
+
+// Ans 6
+
+// 34.output?
+// let b = 5;
+// let c = b++;
+// console.log(b ,c);
+
+// Ans (6,5)
+
+// 35.output?
+// let x = 5;
+// let y = ++x;
+// console.log(x ,y);
+
+// Ans (6,6)
+
+// 36.output?
+// let m = 3;
+// let n = m--;
+// console.log(m ,n);
+
+// Ans (2,3)
+
+// section 8:comparision&Logiacal(37-39)
+// 37.output?
+// console.log(5 == "5");
+
+// Ans true
+
+// 38 output?
+// console.log(5 === "5");
+
+// Ans false
+
+// 39.output
+// console.log(true && false || true);
+
+// Ans true
+
+// section 9:Ternary(40)
+// 40.output?
+// 5<3 ? console.log("Yes"):console.log("No");
+
+// Ans No

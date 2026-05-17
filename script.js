46613b19f71cfaa027054f467dda85c6b1c93a42
MohamedThanveer1 <thanveermettur@gmail.com>
2026-05-17 22:29:54 +0530

    Added local project files

---
+++ script.js
@@ -0,0 +1,55 @@
+// 1.Student attendence System
+
+// let presentStudents = ['Monica' ,'Guna' ,'Mary'];
+// let absentStudents = ['Parvin' ,'Suresh' ,'Angelina'];
+
+// let allStudents = [...presentStudents ,...absentStudents];
+// allStudents.push('kurnal');
+// console.log(allStudents);
+
+// 2.E-Commerce Cart
+// let mobileDetails = {
+//     brand:'Samsung',
+//     model:'Galaxy J7',
+//     price:'12000'
+// }
+
+// let chargerDetails = {
+//     chargeType: 'Type-C',
+//     woltage: '60W',
+//     isWireless: true
+// }
+
+// let cartItem = {
+//     ...mobileDetails,
+//     ...chargerDetails
+// }
+
+// cartItem.deliveryDate = '2026-06-17';
+// console.log(cartItem);
+ 
+// 3.Food Delivery App
+// function orderFood(...items){
+//     console.log(`Total items ordered: ${items.length}`);
+//     console.log(`First item: ${items[0]}`);
+//     console.log(`Last item: ${items[items.length-1]}`); 
+// }
+// orderFood('Burger' ,'Pizza' ,'FriedRice', 'Mushroom')
+
+// 4.Employee Salary Filter
+// let employee = [
+//     {name:'Ram' ,salary:40000},
+//     {name:'Sam' ,salary:45000},
+//     {name:'Ragu' ,salary:55000},
+//     {name:'Ravi' ,salary:62000}
+// ];
+
+// let highPackage = employee.filter(emp =>emp.salary > 50000);
+// console.log(highPackage);
+
+// 5.Online Games Score Board
+// let scores = [70 ,90 ,110 ,120 ,145];
+
+// let totalScore = scores.reduce((previous ,curentValue) => previous + curentValue);
+// console.log('Total score of all players:' ,totalScore);
+
